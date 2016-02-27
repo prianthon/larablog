67b2ed82d715041e05b63d0f4bcdb6de27e1ab01
Pri Anton Subardio <prianton@MacBook-Pro-Pri.local>
2016-02-27 21:45:42 +0700

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
 /*
@@ -10,7 +11,32 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+ gulp.task("copyfiles", function() {
+
+   gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+      .pipe(gulp.dest("resources/assets/js/"));
+
+   gulp.src("vendor/bower_dl/bootstrap/less/**")
+      .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+   gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+      .pipe(gulp.dest("resources/assets/js/"));
+
+   gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+      .pipe(gulp.dest("public/assets/fonts"));
+ });
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    //mix.sass('app.scss');
+    //combine scripts
+    mix.scripts([
+      'js/jquery.js',
+      'js/bootstrap.js',
+    ],
+    'public/assets/js/admin.js',
+    'resources/assets'
+  );
+
+  //compile less
+  mix.less('admin.less', 'public/assets/css/admin.css');
 });
