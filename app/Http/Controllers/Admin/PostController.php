a270788c48703804c80608597afb8ade66bf68ae
Pri Anton Subardio <prianton@MacBook-Pro-Pri.local>
2016-02-28 21:30:24 +0700

    postc auth

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -10,12 +10,18 @@ use App\Http\Requests\PostCreateRequest;
 use App\Http\Requests\PostUpdateRequest;
 use App\Http\Controllers\Controller;
 use App\Post;
+use Auth;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the posts.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('admin.post.index')
